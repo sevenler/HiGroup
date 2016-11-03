20ac113dc2afe36a47a2db587dfc0c173c730e46
johnnyxyz <johnnyxyz@163.com>
2016-11-02 22:36:13 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+import tornado.auth
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import torndb
+from tornado.options import define, options
+from views.index import IndexView
+
+define("port", default=8002, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [(r"/", IndexView),]
+
+    settings = dict(
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+    debug=True,
+    )
+
+    self.db = torndb.Connection("localhost", "hi_group", user='root', password='root')
+    tornado.web.Application.__init__(self, handlers, **settings)
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
